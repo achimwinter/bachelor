8944dd92d1789fdf55c864d262ca3f00f24c04f3
Achim Winter <achim.winter95@gmail.com>
2019-12-09 22:40:57 +0100

    added Signal

---
+++ Bachelor-desktop/src/main/kotlin/com/example/bachelor/signal/SessionGenerator.kt
@@ -0,0 +1,21 @@
+package com.example.bachelor.signal
+
+import org.whispersystems.libsignal.SignalProtocolAddress
+import org.whispersystems.libsignal.util.KeyHelper
+import java.io.File
+
+
+class SessionGenerator {
+
+    val MOBILE_ADRESS = SignalProtocolAddress("Mobile", 1)
+
+    fun prepareSession() {
+        val identityKeyPair = KeyHelper.generateIdentityKeyPair()
+        val registrationId = KeyHelper.generateRegistrationId(false)
+        val preKeys = KeyHelper.generatePreKeys(0, 100)
+        val signedPreKey = KeyHelper.generateSignedPreKey(identityKeyPair, 5)
+
+
+    }
+
+}
